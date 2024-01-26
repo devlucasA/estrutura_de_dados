81706bda5647b47fe4e71cb93c9dedcca7cdb20c
Lucas Araújo de Oliveira <129886429+devlucasA@users.noreply.github.com>
2024-01-26 15:10:05 -0300

    Add files via upload

---
+++ alg1/estr_/numero4/main.c
@@ -0,0 +1,38 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "Pilha.c"
+#include "Pilha.h"
+
+
+
+int main() {
+
+    Pilha *p = criar();
+
+    Livro livro1 = {"BERSERK", "Kentaro Miura", 1989};
+    Livro livro2 = {"GAME OF THRONES", "David Benioff", 2011};
+    Livro jogo3 = {"ELDEN RING", "Hidetaka Miyazaki", 2024};
+
+    push(p, livro1);
+    push(p, livro2);
+    push(p, jogo3);
+
+    mostrar(p);
+
+    
+    Livro jogoRemovido;
+    pop(p, &jogoRemovido);
+
+    printf("\nREMOCAO\n");
+    printf("Titulo: %s\n", jogoRemovido.titulo);
+    printf("Autor: %s\n", jogoRemovido.autor);
+    printf("Ano de Publicacao: %d\n", jogoRemovido.anoPublicacao);
+
+    printf("\n");
+
+    mostrar(p);
+    
+    return 0;
+}
